42a284ed8a619c06508dd81d931f8915c6f8a1d2
djairofilho <djairodsfilho@gmail.com>
2026-09-23 03:05:35 -0300

    fix(client): configura construtor do RestClient

--- src/main/java/br/insper/templatepi/TemplatePiApplication.java
+++ src/main/java/br/insper/templatepi/TemplatePiApplication.java
@@ -2,6 +2,8 @@ package br.insper.templatepi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 // TODO(PI): renomeie a aplicação e o pacote-base se o enunciado exigir.
 @SpringBootApplication
@@ -9,5 +11,10 @@ public class TemplatePiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplatePiApplication.class, args);
+	}
+
+	@Bean
+	RestClient.Builder restClientBuilder() {
+		return RestClient.builder();
 	}
 }
